c2f76c34ac2250ad14baafb7bd30400aef6243f8
Dan Kashkovsky <dannie.k@me.com>
2015-10-23 12:12:41 +0300

    Camera Controller

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+	
+	public Transform Player;
+	public float Distance = 10f;
+	public float Height = 10f;
+	
+	void Update() {
+		transform.position = new Vector3 (Player.position.x, Player.position.y + Height, -Distance);
+		transform.LookAt (Player);
+	}
+}
+
