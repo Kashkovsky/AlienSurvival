ef0bb5b6e0f96a9f48c8f8e4e4044e362294a494
Dan Kashkovsky <dannie.k@me.com>
2015-10-23 12:42:00 +0300

    Enemy Movement Controller

---
+++ Assets/Scripts/Enemy/EnemyMoveController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMoveController : MonoBehaviour {
+
+	Transform player;
+	NavMeshAgent nav;
+	EnemyHealth health;
+	void Awake() {
+		health = GetComponent<EnemyHealth> ();
+		player = GameObject.Find ("Player").transform;
+		nav = GetComponent<NavMeshAgent> ();
+	}
+	
+	void Update() {
+		if (player != null)
+			nav.SetDestination (player.position);
+	}
+
+}
