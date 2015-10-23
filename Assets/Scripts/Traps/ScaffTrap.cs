73fb71f391c2ca7d0ca1b64f3952a098f8e3190e
Dan Kashkovsky <dannie.k@me.com>
2015-10-23 17:16:37 +0300

    Scaffolding Trap

---
+++ Assets/Scripts/Traps/ScaffTrap.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScaffTrap : MonoBehaviour {
+
+	public float Force = 10f;
+	public int Damage = 5;
+	void OnTriggerEnter (Collider other) {
+		if (other.gameObject.tag == "Player") {
+			other.GetComponent<Animator>().applyRootMotion = false;	
+			other.GetComponent<PlayerHealthController>().TakeDamage(Damage, transform.position);
+			other.GetComponent<Rigidbody>().AddForce(new Vector3(0f, Force, 0f), ForceMode.Impulse);
+		}
+	}
+	
+	void OnTriggerExit(Collider other) {
+		if (other.gameObject.tag == "Player") {
+			other.GetComponent<PlayerHealthController>().enabled = true;
+		}
+	}
+
+}
