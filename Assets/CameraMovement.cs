146f0def7ed397f5c34c1bb36da2e3d374429fa1
Dan Kashkovsky <dannie.k@me.com>
2015-10-24 14:17:26 +0300

    Camera

---
+++ Assets/CameraMovement.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+
+	public Transform Player;
+	public float Distance = 10f;
+	public float Height = 10f;
+
+	void Update() {
+		transform.position = new Vector3 (Player.position.x, Player.position.y + Height, -Distance);
+		transform.LookAt (Player);
+	}
+}
