8df711d4a6fe9d8bf72fb8b7cccc95bbb576a36a
Dan Kashkovsky <dannie.k@me.com>
2015-10-24 14:35:09 +0300

    Enemy nav

---
+++ Assets/EnemyMovementController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovementController : MonoBehaviour {
+
+	Transform player;
+	NavMeshAgent nav;
+	
+	void Awake() {
+		player = GameObject.Find ("Player").transform;
+		nav = GetComponent<NavMeshAgent> ();
+	}
+
+	void Update() {
+		if (player != null) {
+			nav.SetDestination (player.position);
+		} else {
+			nav.enabled = false;
+		}
+	}
+}
